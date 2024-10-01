0e69f2ae4a7765be4be796f588fad864a4e58e3f
Denovocto <jejoshadez@gmail.com>
2024-10-01 15:17:53 -0400

    fix: enable unit bug

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@ mkdir -p $HOME/applications/minecraft/data
 mkdir -p $HOME/applications/minecraft/config
 cp -r ./scripts/* $HOME/applications/minecraft
 systemctl enable startup-minecraft-server.service
-systemctl enable backup-minecraft-server.service
 systemctl enable backup-minecraft-server.timer
