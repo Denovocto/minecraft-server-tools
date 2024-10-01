30fa8eaecb31fc09d19aad84ac1dd3098ecc2528
Denovocto <jejoshadez@gmail.com>
2024-10-01 15:16:23 -0400

    fix: bugs

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ cp -r ./systemd-units/* /etc/systemd/system/
 mkdir -p $HOME/applications/minecraft/data
 mkdir -p $HOME/applications/minecraft/config
 cp -r ./scripts/* $HOME/applications/minecraft
+systemctl enable startup-minecraft-server.service
+systemctl enable backup-minecraft-server.service
+systemctl enable backup-minecraft-server.timer
