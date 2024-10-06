544450a0c037f94bd8b7c0c89799ce0ed0592e24
Denovocto <jejoshadez@gmail.com>
2024-10-06 09:33:04 -0400

    fix: user install of podman container startup using systemd

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
-cp -r ./systemd-units/* /etc/systemd/system/
-mkdir -p $HOME/applications/minecraft/data
-mkdir -p $HOME/applications/minecraft/config
-mkdir -p $HOME/applications/grafana/data
-mkdir -p $HOME/applications/prometheus
+mkdir -p $HOME/applications/minecraft/data/
+mkdir -p $HOME/applications/minecraft/config/
+mkdir -p $HOME/applications/grafana/data/
+mkdir -p $HOME/applications/prometheus/
+cp -r ./systemd-units/* $HOME/.config/systemd/user
 cp -r ./minecraft/* $HOME/applications/minecraft
 cp -r ./prometheus/* $HOME/applications/prometheus
 cp -r ./grafana/* $HOME/applications/grafana
-systemctl enable startup-minecraft-server.service
-systemctl enable startup-grafana.service
-systemctl enable startup-prometheus.service
-systemctl enable backup-minecraft-server.timer
+systemctl enable --user startup-minecraft-server.service
+systemctl enable --user startup-grafana.service
+systemctl enable --user startup-prometheus.service
+systemctl enable --user backup-minecraft-server.timer
+
+systemctl --user daemon-reload
